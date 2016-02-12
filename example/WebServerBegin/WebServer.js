2972420b4a718e97252356fb4b500788b79369a7
tocology <hwangjun7777@gmail.com>
2016-02-12 22:45:52 +0900

    feat: Add Static Webserver Example; Promises implemented

--- example/WebServerBegin/WebServer.js
+++ example/WebServerBegin/WebServer.js
@@ -14,39 +14,48 @@ let mimes = {
     '.png': 'image/png'
 }
 
-function webserver(req, res) {
-    // if the route requested is '/', then load 'index.htm' or else
-    // load the requested file(s)
-    
-    let baseURI = url.parse(req.url);
-    let filepath = __dirname + (baseURI.pathname === '/' ? '/index.htm' : baseURI.pathname);
-    
-    // Check if the requested fild is accessible or not
-    fs.access(filepath, fs.F_OK, error => {
-        if(!error) {
-            // Read and Serve the file over response
-            fs.readFile(filepath, (error, content) => {
-                if(!error) {
-                    console.log('Serving: ', filepath);
-                    // Resolve the content type
-                    let contentType = mimes[path.extname(filepath)]; // ex. mimes['.css'] === 'text/css'
-                    // Serve the file from the buffer
-                    res.writeHead(200, {'Content-type': contentType});
-                    res.end(content, 'utf-8');
-                } else {
-                    // Serve a 500
-                    res.writeHead(500);
-                    res.end('The server could not read the file requested');
-                }
-            });
-        } else {
-            // Serve a 404
-            res.writeHead(404);
-            res.end('Content not found!');
-        }
+function fileAccess(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.access(filepath, fs.F_OK, error => {
+            if(!error) {
+                resolve(filepath);
+            } else {
+                reject(error);
+            }
+        });
     });
 }
 
-http.createServer(webserver).listen(3000, () => {
-    console.log('Webserver running on port 3000');
-});
+function fileReader(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, (error, content) => {
+            if(!error) {
+                resolve(content);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
+
+function webserver(req, res) {
+    // if the route requested is '/', then load 'index.htm' or else
+    // load the requested file(s)
+
+    let baseURI = url.parse(req.url);
+    let filepath = __dirname + (baseURI.pathname === '/' ? '/index.htm' : baseURI.pathname);
+    let contentType = mimes[path.extname(filepath)]; // ex. mimes['.css'] === 'text/css'
+
+    fileAccess(filepath)
+        .then(fileReader)
+        .then(content => {
+            res.writeHead(200, {'Content-type': contentType});
+            res.end(content, 'utf-8');
+        })
+        .catch(error => {
+            res.writeHead(404);
+            res.end(JSON.stringify(error));
+        });
+}
+
+http.createServer(webserver).listen(3000, () => console.log('Webserver running on port 3000'));

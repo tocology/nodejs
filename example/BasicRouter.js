0dbec4c702eb4e68644d6daf4c28789d2745f4ae
tocology <hwangjun7777@gmail.com>
2016-04-04 23:29:09 +0900

    chore: Mergy

--- example/BasicRouter.js
+++ example/BasicRouter.js
@@ -1,63 +1,63 @@
-'use strict';
+'use strict'
 
-const http  = require("http");
-const url   = require("url"); // parse url & get more information about user request
-const qs    = require("querystring");
+const http  = require("http")
+const url   = require("url") // parse url & get more information about user request
+const qs    = require("querystring")
 
 let routes = {
     'GET' : {
         '/' : (req, res) => {
-            res.writeHead(200, {'content-type': 'text/html'});
+            res.writeHead(200, {'content-type': 'text/html'})
             res.end('<h1>Hello Nodejs</h1>')
         },
         '/about' : (req, res) => {
-            res.writeHead(200, {'content-type': 'text/html'});
-            res.end('<h1>This is the about page</h1>');
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.end('<h1>This is the about page</h1>')
         },
         '/api/getinfo' : (req, res) => {
             // fetch data from db and respond as JSON
-            res.writeHead(200, {'content-type': 'application/json'});
-            res.end(JSON.stringify(req.queryParams));
+            res.writeHead(200, {'content-type': 'application/json'})
+            res.end(JSON.stringify(req.queryParams))
         }
     },
     'POST' : {
         '/api/logon' : (req, res) => {
-            let body = '';
+            let body = ''
             req.on('data', data => {
-                body += data;
+                body += data
                 if(body.length > 2097152) {
-                    res.writeHead(413, {'Content-type': 'text/html'});
-                    res.end('<h3>Error: The file being uploaded exceeds the 2MB limit</h3>');
-                    req.connection.destroy(); // Stop the progress
+                    res.writeHead(413, {'Content-type': 'text/html'})
+                    res.end('<h3>Error: The file being uploaded exceeds the 2MB limit</h3>')
+                    req.connection.destroy() // Stop the progress
                 }
-            });
+            })
             req.on('end', () => {
-                let params = qs.parse(body);
-                console.log('Username: ', params['username']);
-                console.log('Password: ', params['password']);
+                let params = qs.parse(body)
+                console.log('Username: ', params['username'])
+                console.log('Password: ', params['password'])
                 // Query a db to see if the user exists
                 // If so, send a JSON response to the SPA
-                res.end();
+                res.end()
             })
         }
     },
     'NA' : (req,res) => {
-        res.writeHead(404);
-        res.end('Content not found!');
+        res.writeHead(404)
+        res.end('Content not found!')
     }
 }
 
 function router(req, res) {
-    let baseURI = url.parse(req.url, true);
-    let resolveRoute = routes[req.method][baseURI.pathname];
+    let baseURI = url.parse(req.url, true)
+    let resolveRoute = routes[req.method][baseURI.pathname]
     if(resolveRoute != undefined){
-        req.queryParams = baseURI.query;
-        resolveRoute();
+        req.queryParams = baseURI.query
+        resolveRoute()
     } else {
-        routes['NA'](req, res);
+        routes['NA'](req, res)
     }
 }
 
 http.createServer(router).listen(3000, () => {
-    console.log('Server is on port 3000');
-});
+    console.log('Server is on port 3000')
+})
--- example/WebServerBegin/WebServer.js
+++ example/WebServerBegin/WebServer.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-const http  = require('http');
-const url   = require('url');
-const fs    = require('fs');
-const path  = require('path');
+const http  = require('http')
+const url   = require('url')
+const fs    = require('fs')
+const path  = require('path')
 
 let mimes = {
     '.htm': 'text/html',
@@ -18,46 +18,42 @@ function fileAccess(filepath) {
     return new Promise((resolve, reject) => {
         fs.access(filepath, fs.F_OK, error => {
             if(!error) {
-                resolve(filepath);
+                resolve(filepath)
             } else {
-                reject(error);
+                reject(error)
             }
-        });
-    });
+        })
+    })
 }
 
 function streamFile(filepath) {
     return new Promise((resolve, reject) => {
-        let fileStream = fs.createReadStream(filepath);
+        let fileStream = fs.createReadStream(filepath)
 
         fileStream.on('open', () => {
-            resolve(fileStream);
-        });
+            resolve(fileStream)
+        })
 
         fileStream.on('error', error => {
-            reject(error);
-        });
-    });
+            reject(error)
+        })
+    })
 }
 
 function webserver(req, res) {
     // if the route requested is '/', then load 'index.htm' or else
     // load the requested file(s)
-    let baseURI = url.parse(req.url);
-    let filepath = __dirname + (baseURI.pathname === '/' ? '/index.htm' : baseURI.pathname);
-    let contentType = mimes[path.extname(filepath)]; // ex. mimes['.css'] === 'text/css'
-
     fileAccess(filepath)
         .then(streamFile)
         .then(fileStream => {
-            res.writeHead(200, {'Content-type': contentType});
-            //res.end(content, 'utf-8');
-            fileStream.pipe(res);
+            res.writeHead(200, {'Content-type': contentType})
+            //res.end(content, 'utf-8')
+            fileStream.pipe(res)
         })
         .catch(error => {
-            res.writeHead(404);
-            res.end(JSON.stringify(error));
-        });
+            res.writeHead(404)
+            res.end(JSON.stringify(error))
+        })
 }
 
-http.createServer(webserver).listen(3000, () => console.log('Webserver running on port 3000'));
+http.createServer(webserver).listen(3000, () => console.log('Webserver running on port 3000'))

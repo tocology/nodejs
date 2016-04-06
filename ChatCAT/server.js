1e1e7a0135b4076606e9e47c4e1fae35a83f4362
JunHo Hwang <hwangjun7777@gmail.com>
2016-04-06 13:09:18 +0000

    feat: Add chatCat features; routes, helpers

--- ChatCAT/server.js
+++ ChatCAT/server.js
@@ -1,3 +1,15 @@
 'use strict'
+const express = require('express')
+const app = express()
+const chatCat = require('./app')
 
-console.log("Welcome to ChatCat")
+app.set('port', process.env.PORT || 3000)
+//app.set('views', './views')
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.use('/', chatCat.router)
+
+app.listen(app.get('port'), () => {
+    console.log("ChatCAT Running on Port: ", app.get('port'))
+})
